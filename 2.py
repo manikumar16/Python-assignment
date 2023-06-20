1b1564d94156453ae1ed382143663d9a3de2a97c
manikumar16 <107190872+manikumar16@users.noreply.github.com>
2023-06-20 10:29:17 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,6 @@
+#this is a single line comment
+"""
+this is a multiple line comment
+it is opened and closed with triple bracelets
+"""
+print('Hello world!')
---
+++ 3.py
@@ -0,0 +1,5 @@
+a=31
+b=True
+c=23.12
+d='Hello'
+print(type(a),type(b),type(c),type(d))
